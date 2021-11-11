b9426fdb7f779ec29e79d4841fca69b25837dcde
huanyinyueye <lightfugitive@gmail.com>
2021-11-11 16:21:15 +0800

    fixed

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const jpg = document.getElementById("img1")
 const olimg = document.getElementById("overimg")
 
 
-function click1(){
-    overlay.style.display="block";
-    olimg.setAttribute('src',img1.src);
+function click1() {
+    push = img1.src;
+    overlay.style.display = "block";
+    olimg.setAttribute('src', push);
 }
-function click2(){
-    overlay.style.display="block";
-    olimg.setAttribute('src',img2.src);
+function click2() {
+    overlay.style.display = "block";
+    olimg.setAttribute('src', img2.src);
 }
-function click3(){
-    overlay.style.display="block";
-    olimg.setAttribute('src',img3.src);
+function click3() {
+    overlay.style.display = "block";
+    olimg.setAttribute('src', img3.src);
 }
-function click4(){
-    overlay.style.display="block";
-    olimg.setAttribute('src',img4.src);
+function click4() {
+    overlay.style.display = "block";
+    olimg.setAttribute('src', img4.src);
 }
 
-function off(){
-    overlay.style.display="none";
+function off() {
+    overlay.style.display = "none";
 }
